4bac1391bb660af7a96b75f3cc2f637b3823fc8c
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-05 16:22:55 -0500

    exerciese

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-$(window).load(function() {
-  $('#loading').hide();
+$(window).load(function () {
+    $('#loading').hide();
 });
 
 /**
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 /**
@@ -13,13 +14,13 @@ sayHello('World');
  */
 const {getMovies, postMovie, patchMovie, deleteMovie} = require('./api.js');
 
-
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-$('#movieList').html('')
-  movies.forEach(({title, rating, id}) => {
-    $('#movieList').append(
-    `<div class="card" style="width: 18rem;">
+function refreshMovie() {
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+        $('#movieList').html('')
+        movies.forEach(({title, rating, id}) => {
+            $('#movieList').append(
+                `<div class="card" style="width: 18rem;">
 <!--        <img src="..." class="card-img-top" alt="...">-->
         <div class="card-body">
         <h5 class="card-title">${title}</h5>
@@ -29,31 +30,22 @@ $('#movieList').html('')
     <a href="#" class="btn btn-primary delete">Delete</a>
     </div>
     </div>`
-    // `<ul class="movieTable"><li>id#${id}</li> <li>${title} </li> <li>rating: ${rating}</li></ul>`
-    )
-  });
-});
-
+                // `<ul class="movieTable"><li>id#${id}</li> <li>${title} </li> <li>rating: ${rating}</li></ul>`
+            )
+        });
+    });
+}
+  refreshMovie();
 const url = '/api/movies';
-postMovie().then((movies) => {
-  $("#btn1").click(function(){
+
+$("#btn1").click(function () {
     let NewMovie = {};
     NewMovie.title = $("#movieName").val();
     NewMovie.rating = $('#ratingID1').val();
-    console.log(NewMovie)
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(NewMovie),
-    };
-  fetch(url, options)
-      .then(/* post was created successfully */)
-      .catch(function (error) {
-        console.log(error)
-      });
-  })
+    postMovie(NewMovie).then((movies) => {
+     console.log(movies);
+     refreshMovie();
+    })
 });
 //
 // //second submit button
