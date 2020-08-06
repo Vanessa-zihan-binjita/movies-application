7f0d5488c56ed917e3d25bfb2d042a220f0f5192
Vanessa Rodriguez <vanessa.v.rodriguez89@gmail.com>
2020-08-06 12:20:56 -0500

    fixed delete button

--- src/index.js
+++ src/index.js
@@ -9,54 +9,57 @@ import sayHello from './hello';
 
 sayHello('World');
 
-import movieKey from './keys.js';
-let moviePoster
-
 
 /**
  * require style imports
  */
 const {getMovie, getMovies, postMovie, patchMovie, deleteMovie} = require('./api.js');
+import {movieKey} from './keys';
+
 
 function refreshMovie() {
     getMovies().then((movies) => {
         console.log('Here are all the movies:');
         $('#movieList').html('')
-        movies.forEach(({title, rating, id}) => {
-$.get(`http://www.omdbapi.com/?apikey=36c1feb3&t=${title}`).done(function(data) {
-     let moviePoster = data.Poster
-            $('#movieList').append(
-                `<div class="card" style="width: 18rem;">
+        movies.forEach(({title, rating, id},idx) => {
+            $.get(`http://www.omdbapi.com/?apikey=${movieKey()}&t=${title}`).done(function (data) {
+                let moviePoster = data.Poster
+                $('#movieList').append(
+                    `<div class="card" style="width: 18rem;">
         <img src="${moviePoster}" class="card-img-top" alt="...">
         <div class="card-body">
         <h5 class="card-title">${title}</h5>
-    <p class="card-text">id#${id}</p>
-    <p class="card-text">rating: ${rating}</p>
+    <p class="card-text" hidden>id#${id}</p>
+    <p class="card-text">Rating: ${rating}</p>
     <a href="#" class="btn btn-primary edit" data-id="${id}">Edit</a>
     <a href="#" class="btn btn-primary delete" data-id="${id}">Delete</a>
     </div>
     </div>`
-            )
-        });
- })
-        $('.edit').click(function () {
-          let id = $(this).attr('data-id')
-          getMovie(id).then((movie) => {
-             $('#ratingID2').val(movie.rating);
-             $("#changeMovieName").val(movie.title);
-             $("#editID").val(movie.id);
-          })
+                )
+                if(idx === movies.length-1){
+                    $('.edit').click(function () {
+                        let id = $(this).attr('data-id')
+                        getMovie(id).then((movie) => {
+                            $('#ratingID2').val(movie.rating);
+                            $("#changeMovieName").val(movie.title);
+                            $("#editID").val(movie.id);
+                        })
+                    })
+                    $('.delete').click(function (e) {
+                        e.preventDefault()
+                        let id = $(this).attr('data-id')
+                        deleteMovie(id).then((movie) => {
+                            console.log(movie)
+                            refreshMovie();
+                        })
+                    })
+                }
+            });
         })
-      $('.delete').click(function () {
-        let id = $(this).attr('data-id')
-        deleteMovie(id).then((movie) => {
-         console.log(movie)
-          refreshMovie();
-        })
-      })
     });
 }
-  refreshMovie();
+
+refreshMovie();
 const url = '/api/movies';
 
 $("#btn1").click(function () {
@@ -64,21 +67,21 @@ $("#btn1").click(function () {
     NewMovie.title = $("#movieName").val();
     NewMovie.rating = $('#ratingID1').val();
     postMovie(NewMovie).then((movies) => {
-     console.log(movies);
-     refreshMovie();
+        console.log(movies);
+        refreshMovie();
     })
 });
 
 //second submit button
-$("#btn2").click(function(){
-  let editMovie = {};
-  editMovie.title = $("#changeMovieName").val();
-  editMovie.rating = $('#ratingID2').val();
-  editMovie.id = $('#editID').val();
-  patchMovie(editMovie, editMovie.id).then((movies) => {
-    console.log(movies)
-    refreshMovie();
-  });
+$("#btn2").click(function () {
+    let editMovie = {};
+    editMovie.title = $("#changeMovieName").val();
+    editMovie.rating = $('#ratingID2').val();
+    editMovie.id = $('#editID').val();
+    patchMovie(editMovie, editMovie.id).then((movies) => {
+        console.log(movies)
+        refreshMovie();
+    });
 });
 
 
